6060dcffdaa82da795b2279dea2ad72623d4baf1
talor-hammond <talorjhammond@gmail.com>
2018-07-06 12:31:18 +1200

    basic Leaderboard

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Board from './Board'
+import LeaderBoard from './LeaderBoard'
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class App extends React.Component {
       <Router>
         <div className='app'>
           <h1>NOOB STONE OF DOOM</h1>
-          <Route exact path="/" component={Board} />
+          <Route exact path="/" component={LeaderBoard} />
         </div>
       </Router>
     )
