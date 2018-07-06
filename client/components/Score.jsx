7a8da98878e22d949adbfd9062660359cf61b33c
talor-hammond <talorjhammond@gmail.com>
2018-07-06 15:41:15 +1200

    final final fatal

---
+++ client/components/Score.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//import {HashRouter as Router, Route} from 'react-router-dom'
+
+const Score = () => (
+  <div className='score'>
+      <h3>Your Score is: really good!</h3>
+    </div>
+
+)
+
+export default Score
